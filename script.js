a3b217a0215799b0ad758746ce5748da4e954bd1
aol6lgh <160764663+aol6lgh@users.noreply.github.com>
2024-06-19 19:35:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let git_hub =document.querySelector(".button_github")
+git_hub.addEventListeners("click", function(){
+    window.location.href = "https://github.com/aol6lgh";
+});
+
+let ins =document.querySelector(".button_ins")
+ins.addEventListeners("click", function(){
+    window.location.href = "https://www.instagram.com/aol6lgh?igsh=MWwycnZjczY2MXZwZQ%3D%3D&utm_source=qr";
+});
+
+let tel =document.querySelector(".button_tel")
+tel.addEventListeners("click", function(){
+    window.location.href = "https://t.me/aol6lgh";
+});
+
+let email =document.querySelector(".button_email")
+email.addEventListeners("click", function(){
+    window.location.href = "";
+});
